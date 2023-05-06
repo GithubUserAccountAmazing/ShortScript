7cdcbb2042e42e6108187eb667f45e3f05031695
Rich K. MLS <105183376+RichKMLS@users.noreply.github.com>
2023-05-07 01:11:30 +0200

    Remove multi-line comments from python code
    This commit adds support for multi-line comments in the script. Previously, the script only handled single-line comments starting with '#' and "//". With this change, the script can now also handle multi-line python comments enclosed within triple quotes ('"""'). This is achieved by introducing a new boolean variable 'inComment' to keep track of whether the current line is inside a multi-line comment or not. The script then skips lines that are inside a multi-line comment.

--- ShortScript.sh
+++ ShortScript.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 # A tool to optimize code for AI prompting.
 # Author: github.com/richkmls
-# Date: 5/3/2023
+# Last Updated: 5/6/2023
 # Usage:
 #	0. In ubuntu, add the path to this script as a custom key binding
 # 	1. Select a JavaScript/Python code that you want to shorten.
 # 	2. Press Win+C to copy a markdown codeblock with the shortened code.
 # 	3. Paste the codeblock wherever you want.
-
 
 xclip -selection primary -o | xclip -selection clipboard -i
 
@@ -16,6 +15,9 @@ clipboardText=$(xclip -selection clipboard -o)
 
 # Initialize an empty string for the new text
 newText=""
+
+# Initialize a variable to keep track of whether we are in a multi-line comment
+inComment=false
 
 # Loop through each line of the clipboard text
 while read -r line; do
@@ -28,10 +30,34 @@ while read -r line; do
 		continue
 	fi
 
+    # Check if we are in a multi-line comment
+    if [[ $inComment == true ]]; then
+        # If we are in a multi-line comment, check if this line ends it
+        if [[ $line =~ .*\"\"\".* ]]; then
+            inComment=false
+        fi
+
+        # Skip this line since it is part of a multi-line comment
+        continue
+    else
+        # If we are not in a multi-line comment, check if this line starts one
+        if [[ $line =~ .*\"\"\".* ]]; then
+            inComment=true
+
+            # Skip this line since it is part of a multi-line comment
+            continue
+        fi
+    fi
+
 	# Split the line by // and # and keep everything before it
 	# Use awk to match the pattern and print the first field
 	# ignore instances of "//" that are a part of a URL
 	linePart=$(echo "$line" | awk '{sub(/[^:]\/\//,""); sub(/#.*/,"")}1')
+
+    # Check if line is within quotes and if so, do not split it
+    if [[ $line =~ ^\".*\"$ || $line =~ ^\'.*\'$ ]]; then
+        linePart=$line
+    fi
 
 	# Append the line part to the new text and trim any white space at both ends
 	newText+=$(echo "$linePart" | sed 's/^[ \t]*//;s/[ \t]*$//')
@@ -47,4 +73,3 @@ newText+=$"\`\`\`"
 
 # Replace the clipboard with the new text
 echo "$newText" | xclip -selection clipboard
-
