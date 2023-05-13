666f4bd672b11a68c8bc846a8b4787065d8aa822
Rich K. MLS <105183376+RichKMLS@users.noreply.github.com>
2023-05-13 05:41:59 +0200

    handle comments better
    This command uses sed to perform a regular expression search and replace on the contents of the variable $line.
    The regular expression searches for two patterns separated by an OR (|) operator.
    The first pattern searches for a string that starts and ends with parentheses and contains a '#' symbol between two sets of characters that are not single or double quotes.
    The second pattern searches for a string that starts and ends with parentheses and contains '//' between two sets of characters that are not single or double quotes.
    Both patterns are then replaced with an empty string, effectively removing them from the contents of $line.
        linePart=$(echo "$line" | sed -E 's/\(\(\([^"'"'"']\)#\([^"'"'"']\)\)\)|\(\(\([^"'"'"']\)\/\/\([^"'"'"']\)\)\)//g')

--- ShortScript.sh
+++ ShortScript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # A tool to optimize code for AI prompting.
 # Author: github.com/richkmls
-# Date: 5/7/2023
+# Date: 5/12/2023
 # Usage:
 #	0. In ubuntu, add the path to this script as a custom key binding
 # 	1. Select a JavaScript/Python code that you want to shorten.
@@ -20,7 +20,6 @@ newText=""
 # Initialize a variable to keep track of whether we are in a multi-line comment
 inComment=false
 
-# Loop through each line of the clipboard text
 while read -r line; do
 
     # Remove any leading or trailing whitespace and tabs from the line
@@ -31,11 +30,20 @@ while read -r line; do
         continue
     fi
 
-    # Check if the line contains triple quotes indicating a multi-line comment
-    if [[ $line =~ .*\"\"\".* ]]; then
-        # Check if there is another set of triple quotes on the same line
-        if [[ $line =~ .*\"\"\".*\"\"\".* ]]; then
-            # This is a single line comment using triple quotes syntax
+    # Check if we are currently inside a multi-line comment
+    if [[ $inComment == true ]]; then
+        # Check if the line contains the end of the multi-line comment
+        if [[ $line =~ .*\"\"\".* || $line =~ .*\*\/.* ]]; then
+            inComment=false
+        fi
+        continue
+    fi
+
+    # Check if the line contains triple quotes or /* indicating a multi-line comment
+    if [[ $line =~ .*\"\"\".* || $line =~ .*/\*.* ]]; then
+        # Check if there is another set of triple quotes or */ on the same line
+        if [[ $line =~ .*\"\"\".*\"\"\".* || $line =~ .*/\*.*\*/.* ]]; then
+            # This is a single line comment using triple quotes or /* */ syntax
             continue
         else
             # Set inComment to true and skip this line since it is part of a multi-line comment
@@ -44,10 +52,12 @@ while read -r line; do
         fi
     fi
 
-    # Split the line by // and # and keep everything before it using awk to match the pattern 
-    # and print the first field while ignoring instances of "//" that are part of a URL 
-    # and ignoring instances of "#" or "//" that are between single or double quotation marks
-    linePart=$(echo "$line" | awk '{sub(/[^:]\/\//,""); sub(/([^"'"'"'])#([^"'"'"'])/,"\\1\\2")}1')
+    # This command uses sed to perform a regular expression search and replace on the contents of the variable $line.
+    # The regular expression searches for two patterns separated by an OR (|) operator.
+    # The first pattern searches for a string that starts and ends with parentheses and contains a '#' symbol between two sets of characters that are not single or double quotes.
+    # The second pattern searches for a string that starts and ends with parentheses and contains '//' between two sets of characters that are not single or double quotes.
+    # Both patterns are then replaced with an empty string, effectively removing them from the contents of $line.
+    linePart=$(echo "$line" | sed -E 's/\(\(\([^"'"'"']\)#\([^"'"'"']\)\)\)|\(\(\([^"'"'"']\)\/\/\([^"'"'"']\)\)\)//g')
 
     # Append the line part to the new text and trim any white space at both ends using sed command 
     newText+=$(echo "$linePart" | sed 's/^[ \t]*//;s/[ \t]*$//')
